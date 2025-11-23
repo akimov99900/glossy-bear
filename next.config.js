2dc05931dc681356a928d88454543aca9135e456
akimov99900 <89273259+akimov99900@users.noreply.github.com>
2025-11-23 11:07:00 +0300

    Add redirects to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,16 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        // Твоя ссылка из зеленого поля:
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019aafbd-bf04-60a9-694d-c9b3d4772c86', 
+        permanent: false,
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
