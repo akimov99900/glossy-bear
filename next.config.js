a8974a04ec7dffbb417f33048685f475f521b12f
akimov99900 <89273259+akimov99900@users.noreply.github.com>
2025-11-25 23:12:22 +0300

    Add CORS headers to next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,19 @@ const nextConfig = {
         headers: [
           {
             key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin-allow-popups', // <-- ВОТ ЭТА МАГИЧЕСКАЯ СТРОЧКА
+            value: 'same-origin-allow-popups',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'unsafe-none',
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
           },
         ],
       },
